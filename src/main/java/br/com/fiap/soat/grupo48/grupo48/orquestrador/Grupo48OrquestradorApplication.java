e133dbab377a3f34ac7e4eb38b74510cfd996573
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-08-08 23:11:31 -0300

    devops

---
+++ src/main/java/br/com/fiap/soat/grupo48/grupo48/orquestrador/Grupo48OrquestradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.soat.grupo48.grupo48.orquestrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo48OrquestradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo48OrquestradorApplication.class, args);
+	}
+
+}
